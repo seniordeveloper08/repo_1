0c624cf39e7a86c2d670d1d75055be2726a1f5b2
seniordeveloper08 <seniordev1008@gmail.com>
2020-04-05 16:29:23 -0400

    Tiling 1

--- apis/thumbnail.py
+++ apis/thumbnail.py
@@ -4,6 +4,8 @@ from flask import Blueprint, jsonify, request
 import datetime
 from dotenv import load_dotenv
 from datetime import datetime, timezone
+from PIL import Image
+import math
 
 # LOAD CUSTOMIZED PACKAGE
 from app import db, Camera, Thumbnail
@@ -67,16 +69,40 @@ def create_thumbnail_blueprint(blueprint_name: str, resource_type: str, resource
         vod_url = video.replace("*", "/")
         
         if os.path.exists(".{}".format(vod_url)):
-            print("----------------------------------------",vod_url)
             os.remove(".{}".format(vod_url))
                 
         start1 = datetime.strptime(start, '%Y-%m-%dT%H:%M').strftime("%Y-%m-%d %H:%M:%S")
         end1 = datetime.strptime(end, '%Y-%m-%dT%H:%M').strftime("%Y-%m-%d %H:%M:%S")
 
-        print("ORIGIN",start, end, "UTC", start1, end1)
         for item in db.session.query(Thumbnail).filter(Thumbnail.camera_id == camera_id, Thumbnail.time >= start1, Thumbnail.time <= end1).order_by(Thumbnail.id):
             del item.__dict__['_sa_instance_state']
             thumbnails.append(item.__dict__)
-        return jsonify(thumbnails)
+        
+        images = []
+        sources = []
+        for item in thumbnails:
+            img1 = Image.open(item["path"].replace("/share", "./share"))
+            images.append(img1)
+        
+        img_01_size = images[0].size
+
+        new_im = Image.new('RGB', (5*img_01_size[0],6*img_01_size[1]), (250,250,250))
+        j = 0
+        for i in range(math.ceil(len(images)/30)):
+            for k in range(5):
+                for l in range(6):
+                    if(i*30+j < len(images)):
+                        new_im.paste(images[i*30+j], (k*img_01_size[0], l*img_01_size[1]))
+                        thumbnails[i*30+j]["x"] = k
+                        thumbnails[i*30+j]["y"] = l
+                        thumbnails[i*30+j]["source"] = "merged_images{}.png".format(i)
+                    j+=1
+
+            new_im.save("merged_images{}.png".format(i), "PNG")
+            sources.append("merged_images{}.png".format(i))
+            new_im = Image.new('RGB', (5*img_01_size[0],6*img_01_size[1]), (250,250,250))
+            j=0
+
+        return jsonify(sources, thumbnails)
 
     return blueprint
